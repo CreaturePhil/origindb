9e8ffd8f292151693b89ba64e047f6cb22f8abd7
Phil <birkal@outlook.com>
2015-09-27 15:01:23 -0700

    Add tests

--- gulpfile.js
+++ gulpfile.js
@@ -2,7 +2,7 @@ var gulp = require('gulp');
 var jshint = require('gulp-jshint');
 
 gulp.task('default', function () {
-  return gulp.src('./index.js')
+  return gulp.src(['./index.js', 'test/*.js'])
     .pipe(jshint('.jshintrc'))
     .pipe(jshint.reporter('jshint-stylish'));
 });
---
+++ test/index.js
@@ -0,0 +1,86 @@
+var assert = require('assert');
+var eosdb = require('../index');
+var fs = require('fs');
+var rimraf = require('rimraf');
+
+function readJSON(cb) {
+  'use strict';
+
+  setTimeout(() => {
+    var data = JSON.parse(fs.readFileSync('db/foo.json'));
+    cb(data);
+  }, 20);
+}
+
+describe('eosdb', () => {
+  'use strict';
+
+  var db;
+
+  beforeEach(() => {
+    rimraf.sync('db');
+  });
+
+  after(() => {
+    rimraf.sync('db');
+  });
+
+  describe('CRUD', () => {
+
+    beforeEach(() => {
+      db = eosdb('db');
+    });
+
+    it('creates', (done) => {
+      db('foo').bar = 1;
+      db.save();
+      assert.deepEqual(db('foo'), {bar: 1});
+      readJSON((data) => {
+        assert.deepEqual(data, {bar: 1});
+        done();
+      });
+    }); 
+
+    it('reads', (done) => {
+      db('foo').bar = 1;
+      db.save();
+      assert.equal(db('foo').bar, 1);
+      readJSON((data) => {
+        assert.deepEqual(data, {bar: 1});
+        done();
+      });
+    });
+
+    it('updates', (done) => {
+      db('foo').bar = 1;
+      db.save(); 
+      readJSON((data) => {
+        assert.deepEqual(data, {bar: 1});
+        db('foo').bar = db('foo').bar + 1;
+        db.save();
+        readJSON((data) => {
+          assert.deepEqual(data, {bar: 2});
+          assert.equal(db('foo').bar, 2);
+          done();
+        });
+      });
+    });
+
+    it('deletes', (done) => {
+      db('foo').bar = 1;
+      db.save();
+      readJSON((data) => {
+        assert.deepEqual(data, {bar: 1});
+        delete db('foo').bar;
+        db.save();
+        readJSON((data) => {
+          assert.deepEqual(data, {});
+          assert.deepEqual(db('foo'), {});
+          done();
+        });
+      });
+    });
+
+  });
+
+});
