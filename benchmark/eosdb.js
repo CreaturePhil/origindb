00d01a5a3db2c73673b8fa29033e2b71b625cc08
Phil <birkal@outlook.com>
2015-09-27 21:08:23 -0700

    Test out microtime

--- benchmark/eosdb.js
+++ benchmark/eosdb.js
@@ -1,9 +1,35 @@
+var assert = require('assert');
 var eosdb = require('../index');
 var rimraf = require('rimraf');
 
-suite('eosdb', function() {
+suite('eosdb inserts', function() {
   var db;
   var counter = 0;
+
+  before(function() {
+    db = eosdb('db');  
+  });
+
+  bench('basic inserts', function(next) {
+    counter++;
+    db('str')[''+counter] = ''+counter;
+    db('num')[counter] = counter;
+    db.save(function() {
+      next();
+    });
+  });
+
+  after(function() {
+    rimraf.sync('db'); 
+  });
+
+});
+
+suite('eosdb gets', function() {
+  var db;
+  var counter = 0;
+
+  set('iterations', 10000);
 
   before(function() {
     db = eosdb('db');  
