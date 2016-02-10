9c9400cdf34c8abafed3af84c534afc8826599cf
Phil <birkal@outlook.com>
2016-02-09 19:47:51 -0800

    Use lodash loops

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+'use strict';
+
 var _ = require('lodash');
 var fs = require('graceful-fs');
 var jph = require('json-parse-helpfulerror');
@@ -5,8 +7,6 @@ var path = require('path');
 var steno = require('steno');
 
 module.exports = function(dbDir) {
-  'use strict';
-
   if (!dbDir || typeof dbDir !== 'string') {
     throw new Error("Must provide a directory.");
   }
@@ -22,31 +22,35 @@ module.exports = function(dbDir) {
     }
   }
 
-  fs.readdirSync(dbDir).filter(function(file) {
-    return file.indexOf('.json') >= 0 && file.indexOf('~') < 0;
-  }).forEach(function(file) {
-    var fileDir = path.join(dbDir, file);
-    var fileName = file.slice(0, -5);
-    var data = (fs.readFileSync(fileDir, 'utf-8') || '').trim();
+  var dir = fs.readdirSync(dbDir);
 
-    try {
-      cacheObject[fileName] = jph.parse(data);
-      checksums[fileName] = JSON.stringify(cacheObject[fileName]);
-    } catch (e) {
-      if (e instanceof SyntaxError) e.message = 'Malformed JSON in file: ' + file + '\n' + e.message;
-      throw e;
-    }
-  });
+  _.chain(dir)
+   .filter(function(file) {
+    return file.indexOf('.json') >= 0 && file.indexOf('~') < 0;
+   })
+   .forEach(function(file) {
+      var fileDir = path.join(dbDir, file);
+      var fileName = file.slice(0, -5);
+      var data = (fs.readFileSync(fileDir, 'utf-8') || '').trim();
+
+      try {
+        cacheObject[fileName] = jph.parse(data);
+        checksums[fileName] = JSON.stringify(cacheObject[fileName]);
+      } catch (e) {
+        if (e instanceof SyntaxError) e.message = 'Malformed JSON in file: ' + file + '\n' + e.message;
+        throw e;
+      }
+   });
 
   function save() {
-    for (var file in cacheObject) {
+    _.forIn(cacheObject, function(value, file) {
       var jsonData = JSON.stringify(cacheObject[file]);
 
-      if (jsonData === checksums[file]) continue;
+      if (jsonData === checksums[file]) return;
 
       checksums[file] = jsonData;
       write(file, cacheObject[file]);
-    }
+    });
   }
 
   function write(file, json, cb) {
